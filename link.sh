9af31773b895d7c1795569e1d2952ef334ba9e37
erinwatson <erin.watson@oculus.com>
2018-02-26 22:32:39 -0800

    additional commit

--- link.sh
+++ link.sh
@@ -4,6 +4,7 @@ ln -s ${PWD}/bashrc ~/.bashrc.local
 echo "source .bashrc.local" >> ~/.bashrc
 
 ln -s ${PWD}/inputrc ~/.inputrc
-ln -s ${PWD}/tmux.conf ~/tmux.conf
+ln -s ${PWD}/tmux.conf ~/.tmux.conf
 ln -s ${PWD}/vimrc ~/.vimrc
 ln -s ${PWD}/vimrc.bundles ~/.vimrc.bundles
+ln -s ${PWD}/tmuxinator.bash ~/tmuxinator.bash
